d5177824de45425bed5681184b5255fb4300fc49
Aihua1 <3067742375@qq.com>
2018-01-12 22:14:19 +0800

    作用域

---
+++ 项目/pages/HomePage/HomePage.js
@@ -0,0 +1,21 @@
+Page({
+  data:{
+//text:"这是一个页面"
+  },
+  onLoad:function(Options){
+//页面初始化，Options为页面转化时所带来的参数
+  },
+  onReady: function (){
+//页面渲染完成
+  },
+  onHide: function(){
+//页面隐藏
+  },
+  onShow: function(){
+//页面显示
+  },
+  onUnload: function (){
+//页面关闭
+  },
+
+})
